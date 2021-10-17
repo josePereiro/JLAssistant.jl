1944863a747fa139e4828be37a8f1a6801ad91da
josePereiro <jose.pereiro.lab@gmail.com>
2021-10-17 18:58:19 -0400

    Fix Bug

--- src/JLAssistant.jl
+++ src/JLAssistant.jl
@@ -10,13 +10,13 @@ module JLAssistant
     using FilesTreeTools
 
     include("_find_unregistered_pkgs.jl")
+    include("_project_toml_utils.jl")
+    include("_walk_pkgs.jl")
     include("create_pkg_version.jl")
     include("commit_to_registry.jl")
-    include("_project_toml_utils.jl")
     include("precompile_projects.jl")
     include("generate_pkg.jl")
     include("utils.jl")
-    include("_walk_pkgs.jl")
     include("import_pkgs.jl")
     include("update_manifests.jl")
     include("find_imports.jl")
